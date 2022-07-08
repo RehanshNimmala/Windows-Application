39e9e291b8adcecafbafb876f2120b959aafa967
NAVEEN\Naveen kumar <naveennimmala1818@gmail.com>
2022-07-08 08:28:58 -0500

    Assignmenr 4

---
+++ NimmalaAssign4/NimmalaAssign4/HotelReservationForm.Designer.cs
@@ -0,0 +1,199 @@
+﻿
+namespace NimmalaAssign4
+{
+    partial class HotelReservationForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtCustomerName = new System.Windows.Forms.TextBox();
+            this.txtNumberOfDays = new System.Windows.Forms.TextBox();
+            this.txtStatus = new System.Windows.Forms.TextBox();
+            this.lstReservation = new System.Windows.Forms.ListBox();
+            this.btnDisplayReceipt = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.erpReservations = new System.Windows.Forms.ErrorProvider(this.components);
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.errorProvider2 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.erpReservations)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Customer Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Number Of Days";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(32, 158);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(150, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Status (Gold/Platinum)";
+            // 
+            // txtCustomerName
+            // 
+            this.txtCustomerName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtCustomerName.Location = new System.Drawing.Point(156, 63);
+            this.txtCustomerName.Name = "txtCustomerName";
+            this.txtCustomerName.Size = new System.Drawing.Size(168, 22);
+            this.txtCustomerName.TabIndex = 3;
+            this.txtCustomerName.Validating += new System.ComponentModel.CancelEventHandler(this.txtCustomerName_Validating);
+            // 
+            // txtNumberOfDays
+            // 
+            this.txtNumberOfDays.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtNumberOfDays.Location = new System.Drawing.Point(156, 110);
+            this.txtNumberOfDays.Name = "txtNumberOfDays";
+            this.txtNumberOfDays.Size = new System.Drawing.Size(168, 22);
+            this.txtNumberOfDays.TabIndex = 4;
+            this.txtNumberOfDays.Validating += new System.ComponentModel.CancelEventHandler(this.txtNumberOfDays_Validating);
+            // 
+            // txtStatus
+            // 
+            this.txtStatus.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtStatus.Location = new System.Drawing.Point(188, 158);
+            this.txtStatus.Name = "txtStatus";
+            this.txtStatus.Size = new System.Drawing.Size(136, 22);
+            this.txtStatus.TabIndex = 5;
+            // 
+            // lstReservation
+            // 
+            this.lstReservation.Font = new System.Drawing.Font("Courier New", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstReservation.FormattingEnabled = true;
+            this.lstReservation.ItemHeight = 16;
+            this.lstReservation.Location = new System.Drawing.Point(35, 216);
+            this.lstReservation.Name = "lstReservation";
+            this.lstReservation.Size = new System.Drawing.Size(376, 212);
+            this.lstReservation.TabIndex = 6;
+            // 
+            // btnDisplayReceipt
+            // 
+            this.btnDisplayReceipt.Location = new System.Drawing.Point(586, 73);
+            this.btnDisplayReceipt.Name = "btnDisplayReceipt";
+            this.btnDisplayReceipt.Size = new System.Drawing.Size(153, 59);
+            this.btnDisplayReceipt.TabIndex = 7;
+            this.btnDisplayReceipt.Text = "Display Receipt";
+            this.btnDisplayReceipt.UseVisualStyleBackColor = true;
+            this.btnDisplayReceipt.Click += new System.EventHandler(this.btnDisplayReceipt_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.CausesValidation = false;
+            this.btnClear.Location = new System.Drawing.Point(586, 184);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(153, 62);
+            this.btnClear.TabIndex = 8;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(586, 324);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(157, 61);
+            this.btnExit.TabIndex = 9;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // erpReservations
+            // 
+            this.erpReservations.ContainerControl = this;
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // errorProvider2
+            // 
+            this.errorProvider2.ContainerControl = this;
+            // 
+            // HotelReservationForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnDisplayReceipt);
+            this.Controls.Add(this.lstReservation);
+            this.Controls.Add(this.txtStatus);
+            this.Controls.Add(this.txtNumberOfDays);
+            this.Controls.Add(this.txtCustomerName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "HotelReservationForm";
+            this.Text = "Hotel Room Reservations";
+            ((System.ComponentModel.ISupportInitialize)(this.erpReservations)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtCustomerName;
+        private System.Windows.Forms.TextBox txtNumberOfDays;
+        private System.Windows.Forms.TextBox txtStatus;
+        private System.Windows.Forms.ListBox lstReservation;
+        private System.Windows.Forms.Button btnDisplayReceipt;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.ErrorProvider erpReservations;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.ErrorProvider errorProvider2;
+    }
+}
+
