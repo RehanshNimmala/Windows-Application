a9c1ca34adb96b6939f087bfb202be3eee0092e3
NAVEEN\Naveen kumar <naveennimmala1818@gmail.com>
2022-08-27 22:53:56 -0500

    Exit button functionality added

--- TempConverterPractise/TempConverterPractise/Form1.Designer.cs
+++ TempConverterPractise/TempConverterPractise/Form1.Designer.cs
@@ -106,6 +106,7 @@
             this.exitButton.TabIndex = 4;
             this.exitButton.Text = "Exit";
             this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
             // 
             // resultLabel
             // 
