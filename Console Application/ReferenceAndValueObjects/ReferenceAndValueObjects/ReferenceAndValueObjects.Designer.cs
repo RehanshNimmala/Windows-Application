76df6976f1d92a5fe3f1f4d87d5ad7fb3406ffdf
NAVEEN\Naveen kumar <naveennimmala1818@gmail.com>
2022-09-13 23:02:29 -0500

    Reference and Value types

---
+++ Console Application/ReferenceAndValueObjects/ReferenceAndValueObjects/ReferenceAndValueObjects.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace ReferenceAndValueObjects
+{
+    partial class ReferenceAndValueObjects
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.originalValueButton = new System.Windows.Forms.Button();
+            this.callClassButton = new System.Windows.Forms.Button();
+            this.valuesAfterCallButton = new System.Windows.Forms.Button();
+            this.originalLabel = new System.Windows.Forms.Label();
+            this.valuesAfterCallLabel = new System.Windows.Forms.Label();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(137, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(491, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Passing Variables by Values and Objects by Reference";
+            // 
+            // originalValueButton
+            // 
+            this.originalValueButton.Location = new System.Drawing.Point(29, 127);
+            this.originalValueButton.Name = "originalValueButton";
+            this.originalValueButton.Size = new System.Drawing.Size(156, 75);
+            this.originalValueButton.TabIndex = 1;
+            this.originalValueButton.Text = "Original Value";
+            this.originalValueButton.UseVisualStyleBackColor = true;
+            this.originalValueButton.Click += new System.EventHandler(this.originalValueButton_Click);
+            // 
+            // callClassButton
+            // 
+            this.callClassButton.Location = new System.Drawing.Point(320, 127);
+            this.callClassButton.Name = "callClassButton";
+            this.callClassButton.Size = new System.Drawing.Size(164, 75);
+            this.callClassButton.TabIndex = 2;
+            this.callClassButton.Text = "Call Class";
+            this.callClassButton.UseVisualStyleBackColor = true;
+            this.callClassButton.Click += new System.EventHandler(this.callClassButton_Click);
+            // 
+            // valuesAfterCallButton
+            // 
+            this.valuesAfterCallButton.Location = new System.Drawing.Point(545, 127);
+            this.valuesAfterCallButton.Name = "valuesAfterCallButton";
+            this.valuesAfterCallButton.Size = new System.Drawing.Size(173, 75);
+            this.valuesAfterCallButton.TabIndex = 3;
+            this.valuesAfterCallButton.Text = "Values after Class call";
+            this.valuesAfterCallButton.UseVisualStyleBackColor = true;
+            this.valuesAfterCallButton.Click += new System.EventHandler(this.valuesAfterCallButton_Click);
+            // 
+            // originalLabel
+            // 
+            this.originalLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.originalLabel.Location = new System.Drawing.Point(29, 245);
+            this.originalLabel.Name = "originalLabel";
+            this.originalLabel.Size = new System.Drawing.Size(252, 174);
+            this.originalLabel.TabIndex = 4;
+            // 
+            // valuesAfterCallLabel
+            // 
+            this.valuesAfterCallLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.valuesAfterCallLabel.Location = new System.Drawing.Point(417, 245);
+            this.valuesAfterCallLabel.Name = "valuesAfterCallLabel";
+            this.valuesAfterCallLabel.Size = new System.Drawing.Size(301, 174);
+            this.valuesAfterCallLabel.TabIndex = 5;
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(29, 455);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(122, 61);
+            this.exitButton.TabIndex = 6;
+            this.exitButton.Text = "E&xit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // ReferenceAndValueObjects
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 528);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.valuesAfterCallLabel);
+            this.Controls.Add(this.originalLabel);
+            this.Controls.Add(this.valuesAfterCallButton);
+            this.Controls.Add(this.callClassButton);
+            this.Controls.Add(this.originalValueButton);
+            this.Controls.Add(this.label1);
+            this.Name = "ReferenceAndValueObjects";
+            this.Text = "Reference And Value Objects";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button originalValueButton;
+        private System.Windows.Forms.Button callClassButton;
+        private System.Windows.Forms.Button valuesAfterCallButton;
+        private System.Windows.Forms.Label originalLabel;
+        private System.Windows.Forms.Label valuesAfterCallLabel;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ Console Application/ReferenceAndValueObjects/ReferenceAndValueObjects/ReferenceAndValueObjects.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReferenceAndValueObjects
+{
+    //Created by Nimmala
+    //111111111111111111
+    public partial class ReferenceAndValueObjects : Form
+    {
+        //Form level variables
+        static Random myRandom = new Random();
+        // an integer with random number between 0 and 100
+        private int byValueInt = myRandom.Next(0, 100);
+        // An array with three random numbers
+        private int[] byReferenceArray = { myRandom.Next(0, 20), myRandom.Next(30, 50), myRandom.Next(60, 80) };
+        public ReferenceAndValueObjects()
+        {
+            InitializeComponent();
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }// end of exit button
+
+        private void originalValueButton_Click(object sender, EventArgs e)
+        {
+            originalLabel.Text = $@"Original Values are:{Environment.NewLine}byValueInt:{byValueInt}
+               {Environment.NewLine}byReferenceArray:[{byReferenceArray[0]},{byReferenceArray[1]}, {byReferenceArray[2]}]";
+        }//Original Button 
+
+        private void valuesAfterCallButton_Click(object sender, EventArgs e)
+        {
+            valuesAfterCallLabel.Text = $@"Values of variables in for form, after calling class are:
+                {Environment.NewLine} byValueInt:{byValueInt}
+                {Environment.NewLine} byReferenceArray:[{byReferenceArray[0]},{byReferenceArray[1]},{byReferenceArray[2]}]";
+
+        }
+
+        private void callClassButton_Click(object sender, EventArgs e)
+        {
+            ByValueAndByReference myValRef= new ByValueAndByReference();
+            myValRef.ByReferenceMethod(byReferenceArray);
+            myValRef.ByValueMethod(  byValueInt);
+            MessageBox.Show($"ByValueReference class instantiated and /r/n ByReferenceMethod()" +
+                " and ByValueMethod() called.","Class Instantiated",MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        }//end of CallClassButton
+    }//end form code
+        class ByValueAndByReference
+        {
+            public int ByValueMethod(int valueNumber)
+            {
+                return valueNumber =valueNumber* valueNumber;
+            }// End method
+            public void ByReferenceMethod(int[]referenceArray)
+            {
+                for (int i = 0; i < referenceArray.Length; i++)
+                {
+                    //square each element in the array
+                    referenceArray[i] *=referenceArray[i];
+
+                }
+            }// end method ByRefernceMethod
+        }// end of class
+}// end namespace
