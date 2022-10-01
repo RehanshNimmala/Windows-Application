32cd56ebb0bc216f76319c6c4f5b9e90077ad603
NAVEEN\Naveen kumar <naveennimmala1818@gmail.com>
2022-10-01 11:45:53 -0500

    LinkListGenerics

---
+++ LinkedListGeneric/LinkedListGeneric/LinkedListClass.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListGeneric
+{
+    //Created by Nimmala
+    //22222222222222222
+    class LinkedListClass<T> : IEnumerable<T>
+    {
+        //Properties
+        public LLNodeMarker<T> First { get; set; }
+        public LLNodeMarker<T> Last { get; set; }
+
+        //Method
+        public LLNodeMarker<T> AddLast(T node)
+        {
+            //This method accepts an incoming argument of type LLNodeMarker<T>(could be a value of any generic data type)
+
+            //call the LinkNode class and create a new linked list node of the incoming value
+            var newNode = new LLNodeMarker<T>(node);
+
+            //Now set the previous and next node properties of the node.
+            //If this is the first one, then there is no previous or next node.
+            if (this.First == null)
+            {
+                First = newNode;// sets incoming node value as the first node.
+                Last = newNode;// This is the only node, so first and last node is the same.
+            }
+            else
+            {
+                //if this is the not the first node, ther is already one,
+                //make the current node as the previous one,and make the incoming node as the current one.
+                LLNodeMarker<T> previous = Last;
+                Last.NextNode = newNode;//incoming node is attached as the next node to the current last node.
+                Last = newNode;//setting the newly received node as the last node.
+                Last.PreviousNode = previous;//Assigning the node that arrived earlier as the last node because:
+                                             //the previous node to the new node(last node)just got added.
+
+            }// End if/else
+            //Now the new node has the earlier nodes as previous node and the nwe node is the last node now.
+            //Very first arriving node is the first node.
+            //just added one more node to the chain of nodes
+            return newNode;
+
+
+        }//end method AddLast
+        public IEnumerator<T> GetEnumerator()
+        {
+            //This method loops through a given list and returns one node at a time
+            LLNodeMarker<T> current = First;
+            while (current != null)
+            {
+                yield return current.ValueProperty;
+                //Yield return statement returns one node in the loop at a time,without exiting the method
+                //because of the return statement.
+                //Control remains with the method even after the return statement occuring.
+                current = current.NextNode;
+            }// end of While
+        }//end GetEnumerator method
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            //This is the implementation of IEnumerable.GetEnumerator() because the class derives from InterfaceIEnumerable<T>
+            //which implements this method.
+            //IEnumerable interface exposes the Enumerator, which supports a simple iteration over a non-generic collection.
+            return GetEnumerator();
+        }//End GetEnumerator()
+    }//End LinkedListClass
+}
---
+++ LinkedListGeneric/LinkedListGeneric/LinkedListForm.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace LinkedListGeneric
+{
+    partial class LinkedListForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.exitButton = new System.Windows.Forms.Button();
+            this.integerButton = new System.Windows.Forms.Button();
+            this.stringButton = new System.Windows.Forms.Button();
+            this.boolButton = new System.Windows.Forms.Button();
+            this.mixedTypeButton = new System.Windows.Forms.Button();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(69, 373);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(96, 43);
+            this.exitButton.TabIndex = 0;
+            this.exitButton.Text = "E&xit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // integerButton
+            // 
+            this.integerButton.Location = new System.Drawing.Point(69, 40);
+            this.integerButton.Name = "integerButton";
+            this.integerButton.Size = new System.Drawing.Size(96, 43);
+            this.integerButton.TabIndex = 1;
+            this.integerButton.Text = "&Integer";
+            this.integerButton.UseVisualStyleBackColor = true;
+            this.integerButton.Click += new System.EventHandler(this.integerButton_Click);
+            // 
+            // stringButton
+            // 
+            this.stringButton.Location = new System.Drawing.Point(69, 118);
+            this.stringButton.Name = "stringButton";
+            this.stringButton.Size = new System.Drawing.Size(96, 43);
+            this.stringButton.TabIndex = 2;
+            this.stringButton.Text = "&String";
+            this.stringButton.UseVisualStyleBackColor = true;
+            this.stringButton.Click += new System.EventHandler(this.stringButton_Click);
+            // 
+            // boolButton
+            // 
+            this.boolButton.Location = new System.Drawing.Point(69, 201);
+            this.boolButton.Name = "boolButton";
+            this.boolButton.Size = new System.Drawing.Size(96, 43);
+            this.boolButton.TabIndex = 3;
+            this.boolButton.Text = "&Bool";
+            this.boolButton.UseVisualStyleBackColor = true;
+            this.boolButton.Click += new System.EventHandler(this.boolButton_Click);
+            // 
+            // mixedTypeButton
+            // 
+            this.mixedTypeButton.Location = new System.Drawing.Point(69, 280);
+            this.mixedTypeButton.Name = "mixedTypeButton";
+            this.mixedTypeButton.Size = new System.Drawing.Size(96, 43);
+            this.mixedTypeButton.TabIndex = 4;
+            this.mixedTypeButton.Text = "&Mixed Type";
+            this.mixedTypeButton.UseVisualStyleBackColor = true;
+            this.mixedTypeButton.Click += new System.EventHandler(this.mixedTypeButton_Click);
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.resultLabel.Location = new System.Drawing.Point(271, 107);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(440, 230);
+            this.resultLabel.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.mixedTypeButton);
+            this.Controls.Add(this.boolButton);
+            this.Controls.Add(this.stringButton);
+            this.Controls.Add(this.integerButton);
+            this.Controls.Add(this.exitButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Button integerButton;
+        private System.Windows.Forms.Button stringButton;
+        private System.Windows.Forms.Button boolButton;
+        private System.Windows.Forms.Button mixedTypeButton;
+        private System.Windows.Forms.Label resultLabel;
+    }
+}
+
