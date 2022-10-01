75d360a38d2cc23b0dec3352c9d4a408ce681a7c
NAVEEN\Naveen kumar <naveennimmala1818@gmail.com>
2022-09-30 23:13:41 -0500

    Singleton practice

---
+++ SingletonPractise/SingletonPractise/SingletonContainerForm.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SingletonPractise
+{
+    public partial class SingletonContainerForm : Form
+    {
+        public SingletonContainerForm()
+        {
+            InitializeComponent();
+        }//ctor
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            About about = new About();
+            about.MdiParent = this;
+            about.Show();
+        }//about
+
+      
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void SingletonFormToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            //Create an instance using static method
+            SingletonForm singletonInstForm = SingletonForm.SingletonFormInstance();
+            //make it a child
+            singletonInstForm.MdiParent = this;
+            singletonInstForm.Show();
+
+        }
+    }
+}
---
+++ SingletonPractise/SingletonPractise/SingletonForm.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SingletonPractise
+{
+    //Created by Naveen
+    //111111111111111
+    public partial class SingletonForm : Form
+    {
+        private static SingletonForm instance;
+        private SingletonForm()
+        {
+            InitializeComponent();
+        }//ctor
+        internal static SingletonForm SingletonFormInstance()
+        {
+            //This method returns an instance of singletonForm, only of instance is null.
+            if(instance == null)
+                instance= new SingletonForm();
+            return instance;
+        }
+
+        private void SingletonForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // when the form closes,make the instance null.so the form loads again when called for the second time
+            instance=null;
+        }
+
+        private void helloButton_Click(object sender, EventArgs e)
+        {
+            resultLabel.Text=$"Hello{nameTextBox.Text}";
+        }
+    }
+}
