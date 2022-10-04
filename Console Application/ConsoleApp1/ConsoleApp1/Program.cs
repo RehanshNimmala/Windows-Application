32fbd9993371770b8234448703c3f5aa52d641d0
NAVEEN\Naveen kumar <naveennimmala1818@gmail.com>
2022-10-03 23:02:04 -0500

    console project added

---
+++ Console Application/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter your Name");
+            string myName=Console.ReadLine();
+            Console.WriteLine("Please Enter your Age");
+
+            string myAge =Console.ReadLine();
+            Welcome welcome=new Welcome(myName);
+            Welcome awelcome = new Welcome(myAge);
+
+            Console.ReadKey();
+
+        }
+        public class Welcome
+        {
+            public Welcome(string name)
+            {
+                Console.WriteLine($"Welcome {name}");
+            }
+        }
+    }
+}
