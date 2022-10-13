30206f77c7ab592ed56744bbf0496d4e9e91e45f
NAVEEN\Naveen kumar <naveennimmala1818@gmail.com>
2022-10-12 22:53:20 -0500

    Add Variance

---
+++ Console Application/NimmalaWeek9/NimmalaWeek9/MyObject2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NimmalaWeek9
+{
+    //created by Nimmala
+    //333333333333
+
+    internal class MyObject2
+    {
+        public override string ToString()
+        {
+            return $"My Name is: {this.GetType().Name} and value is Naveen Nimmala";
+        }
+    }//class
+}//namespace
---
+++ Console Application/NimmalaWeek9/NimmalaWeek9/MyObject3.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NimmalaWeek9
+{
+    internal class MyObject3
+    {
+        //4444444444444444
+        public string MyProperty { get; set; }
+        public MyObject3(string name)
+        {
+            MyProperty = name;
+        }
+        public override string ToString()
+        {
+            return $"Im an object of type:{this.GetType().Name} and name is {this.MyProperty}";
+        }
+    }//class
+}//namespace
