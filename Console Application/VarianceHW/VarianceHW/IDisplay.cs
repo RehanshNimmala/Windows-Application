f09e96d5ee9a643c2353c6172670581465eaf1b9
NAVEEN\Naveen kumar <naveennimmala1818@gmail.com>
2022-10-20 23:29:01 -0500

    Variance Types

---
+++ Console Application/VarianceHW/VarianceHW/IDisplay.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VarianceHW
+{
+    //creted by Nimmala
+    //222222222222
+    //Contravariant
+    interface IDisplay<inT>
+    {
+        //method
+        void Show(Task item);
+    }
+}
---
+++ Console Application/VarianceHW/VarianceHW/IIndex.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VarianceHW
+{
+    //creted by Nimmala
+    //111111111111
+    //Covariant Interface
+    //Covariance enables you to use a more derived type than originally specified.
+  interface IIndex<out T>
+    {
+        T this[int index] { get;}
+        int count { get; }
+    }
+}
